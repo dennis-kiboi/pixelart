30b4a874a2a99d7e3e4c9e0a503a96f991ec54bd
Dennis Kiboi <mururi.kiboi@gmail.com>
2022-10-03 23:26:21 +0300

    Test API

---
+++ js/index.js
@@ -0,0 +1,7 @@
+const unsplashAPI = "https://api.unsplash.com/photos/?client_id=fN08An0QrEjLNjPclbs5kFfPlXNu8neGbbQ-mBDkxF0"
+window.addEventListener('DOMContentLoaded', (event) => {
+  console.log('DOM fully loaded and parsed');
+  // fetch(unsplashAPI)
+  // .then(res => res.json())
+  // .then(data => console.log(data))
+});
