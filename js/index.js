4d652044a67c6be0a3872b98a9e2cc7be5dc60b0
Dennis Kiboi <mururi.kiboi@gmail.com>
2022-10-08 11:40:01 +0300

    Add images from API

--- js/index.js
+++ js/index.js
@@ -1,7 +1,23 @@
-const unsplashAPI = "https://api.unsplash.com/photos/?client_id=fN08An0QrEjLNjPclbs5kFfPlXNu8neGbbQ-mBDkxF0"
-window.addEventListener('DOMContentLoaded', (event) => {
+const unsplashAPI = "https://api.unsplash.com/photos/?client_id=fN08An0QrEjLNjPclbs5kFfPlXNu8neGbbQ-mBDkxF0";
+const galleryDiv = document.getElementById('row');
+
+function renderImage(imageData) {
+  let image = document.createElement('div');
+  image.className = 'col-lg-4 col-md-6 col-6'
+  image.innerHTML = `
+    <div class="card">
+      <img src=${imageData.urls.small} alt="">
+    </div>
+  `
+
+  galleryDiv.appendChild(image);
+}
+
+window.addEventListener('DOMContentLoaded', () => {
   console.log('DOM fully loaded and parsed');
-  // fetch(unsplashAPI)
-  // .then(res => res.json())
-  // .then(data => console.log(data))
+  fetch(unsplashAPI)
+  .then(res => res.json())
+  .then(data => {
+    data.forEach(imageData => renderImage(imageData))
+  });
 });
